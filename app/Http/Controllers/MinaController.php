3e35285017b42eae47814425bd21524a5ee0da00
Mina SaȜd <73399220+Mina-Saad-2022@users.noreply.github.com>
2023-03-10 20:39:47 +0200

    10-3-2023 problem abache

---
+++ app/Http/Controllers/MinaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mina;
+use Illuminate\Http\Request;
+
+class MinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Mina  $mina
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mina $mina)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Mina  $mina
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mina $mina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Mina  $mina
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mina $mina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Mina  $mina
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mina $mina)
+    {
+        //
+    }
+}
---
+++ database/factories/MinaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Mina;
+use Faker\Generator as Faker;
+
+$factory->define(Mina::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
